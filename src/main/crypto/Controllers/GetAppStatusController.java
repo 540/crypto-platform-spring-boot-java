baed3685655992a0e1ce70b6d6556326f5808c92
Iker Mariñelarena <iker@540deg.com>
2023-03-07 20:41:20 +0100

    Ajustado naming de GetAppStatusController

--- src/main/crypto/Controllers/GetAppStatusController.java
+++ src/main/crypto/Controllers/GetAppStatusController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GetStatusController {
+public class GetAppStatusController {
     @GetMapping("/status")
     public String index() {
         return "Make this work :)";
